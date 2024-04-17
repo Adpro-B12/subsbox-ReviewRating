f31280ac2a8214a8c9c6460fa2d28ea24817afb5
Bayu <rizqi.bayu@ui.ac.id>
2024-04-17 19:43:22 +0700

    Test front end

---
+++ src/main/java/id/ac/ui/cs/advprog/rating/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.rating.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("")
+public class HomePageController {
+    String createHTML = "userCreate";
+    @GetMapping("")
+    @ResponseBody
+    public String createUserPage(Model model) {
+        return "<h1>Test Rating Page sudah berhasil!</h1>";
+    }
+}
