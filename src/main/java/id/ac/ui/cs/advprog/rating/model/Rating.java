4097049c5c00aafbb791972e2dd004c3e71d739c
Bayu <rizqi.bayu@ui.ac.id>
2024-04-24 21:05:57 +0700

    [GREEN] Implement Rating model.

---
+++ src/main/java/id/ac/ui/cs/advprog/rating/model/Rating.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.rating.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter@Getter
+public class Rating {
+    private String ratingId;
+    private String status;
+    private int ratingScore;
+    private String ratingReview;
+    private User user;
+    private SubscriptionBox subscriptionBox;
+}
