3a4fc60526c6a716aa138eceb8958c884132123e
Bayu <rizqi.bayu@ui.ac.id>
2024-05-05 23:53:22 +0700

    [REFACTOR] changing file structure.

--- src/main/java/enums/Status.java
+++ src/main/java/enums/Status.java
@@ -1,4 +1,4 @@
-package id.ac.ui.cs.advprog.rating.enums;
+package enums;
 
 import lombok.Getter;
 
--- src/main/java/id/ac/ui/cs/advprog/rating/model/Rating.java
+++ src/main/java/id/ac/ui/cs/advprog/rating/model/Rating.java
@@ -1,7 +1,6 @@
 package id.ac.ui.cs.advprog.rating.model;
 
-import id.ac.ui.cs.advprog.rating.enums.Status;
-import lombok.Builder;
+import enums.Status;
 import lombok.Getter;
 import lombok.Setter;
 
